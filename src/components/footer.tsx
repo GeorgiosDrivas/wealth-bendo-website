bec3d30142720bbebc4506fef278f7c1eb4b4a58
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-10-19 13:01:23 +0300

    Footer component layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -25,7 +25,45 @@ export default function Footer() {
               </p>
             </div>
           </div>
-          <div className="col-6"></div>
+          <div className="col-6">
+            <div
+              id="footer-nav"
+              className="d-flex justify-content-between align-items-start"
+            >
+              <div className="d-flex flex-column justify-content-center align-items-center footer-single-nav">
+                <h5>MAIN</h5>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Blog</a>
+              </div>
+              <div className="d-flex flex-column justify-content-center align-items-center  footer-single-nav">
+                <h5>PAGES</h5>
+                <a href="#">Contact</a>
+                <a href="#">Terms & Conditions</a>
+                <a href="#">Privacy Policy</a>
+              </div>
+              <div className="d-flex flex-column justify-content-center align-items-center  footer-single-nav">
+                <h5>UTILITIES</h5>
+                <a href="#">Style Guide</a>
+                <a href="#">Instructions</a>
+                <a href="#">Licenses</a>
+                <a href="#">Changelog</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="footer-line"></div>
+        <div className="row">
+          <div className="col-6">
+            <p>© 2024 Wealth Bento. All Rights Reserved.</p>
+          </div>
+          <div className="col-6">
+            <div className="d-flex justify-content-between align-items-center">
+              <p>Powered By Webflow Built By Rick Mummery</p>
+              <p>Built By Rick Mummery</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
