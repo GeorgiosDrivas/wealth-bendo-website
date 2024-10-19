e8908f3429d6eb8997eb49f4d52182be7267a0e2
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-10-19 12:01:19 +0300

    Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+import "../styles/header.styles.css";
+
+export default function Header() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <div id="logo">
+              <img src="./assets/logo.png" alt="Logo" />
+            </div>
+          </div>
+          <div className="col-6">
+            <ul
+              id="header-nav"
+              className="d-flex justify-content-around align-items-center"
+            >
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>SERVICES</li>
+              <li>BLOG</li>
+              <li>CONTACT</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
